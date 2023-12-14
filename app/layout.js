7443875f585f0c21aeb423dbb882684cadadecb2
Omar Ventura <ventura@MacBook-Pro-de-Omar.local>
2023-12-13 23:17:14 -0600

    Agregando Soeed Insights de vercel

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,8 @@ import "bootstrap/dist/css/bootstrap.css";
 import { Inter } from 'next/font/google'
 import './globals.css'
 import AppProvider from './context/appProvider'
+import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from '@vercel/analytics/react';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -14,9 +16,12 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
     <AppProvider>
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children}
+        <SpeedInsights />
+        <Analytics />
+      </body>
     </AppProvider>
-      
     </html>
   )
 }
